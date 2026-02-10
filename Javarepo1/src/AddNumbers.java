08b696b0a9822a483f867ece24d12ce4909b6753
Vansh Pitale <vanssh24@gmail.com>
2026-02-10 14:51:53 +0530

    Delete Javarepo1/src/AddNumbers.java

--- Javarepo1/src/AddNumbers.java
+++
@@ -1,7 +0,0 @@
-public class AddNumbers {
-    public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println("Sum = " + (a + b));
-    }
-}
